f826885ab07fda3b1484b41233716284ecf2e76e
Mennaelsamadisy <mennaelsamadisy@gmail.com>
2025-06-12 16:25:24 +0300

    v2.1

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ app.post("/forgotPassword", async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "",
-        pass: "",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
