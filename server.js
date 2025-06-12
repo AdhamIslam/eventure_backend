1de35a9f57c1f9c850d01446205b30d955a682ce
AdhamIslam <adhamislam088@gmail.com>
2025-06-13 01:30:03 +0300

    v2.4

--- server.js
+++ server.js
@@ -293,7 +293,7 @@ app.post("/verify-email", async (req, res) => {
 app.get("/getClientProfileById/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query("SELECT * FROM client WHERE id = $1", [id]);
+    const result = await pool.query("SELECT * FROM client WHERE client_id = $1", [id]);
     if (result.rows.length === 0) return res.status(404).json({ error: "User not found" });
     res.status(200).json(result.rows[0]);
   } catch (err) {
