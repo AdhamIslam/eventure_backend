4333ff33916ac258fbb67e757f1b16ad440004b2
AdhamIslam <adhamislam088@gmail.com>
2025-06-13 01:14:28 +0300

    v2.3

--- server.js
+++ server.js
@@ -290,7 +290,17 @@ app.post("/verify-email", async (req, res) => {
 });
 
 
-
+app.get("/getClientProfileById/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM client WHERE id = $1", [id]);
+    if (result.rows.length === 0) return res.status(404).json({ error: "User not found" });
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 //**************************************************************************planner****************************************************************** */
 
 app.post("/plannerLoginValidate",async(req,res)=>{
