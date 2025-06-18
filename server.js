850039b8678cf4de98bee812b974b51206727230
AdhamIslam <adhamislam088@gmail.com>
2025-06-18 19:29:33 +0300

    V6.1.9

--- server.js
+++ server.js
@@ -642,7 +642,7 @@ app.get("/detailedEvents", async (req, res) => {
 });
 
 app.get("/plannerEvents", async (req, res) => {
-   const plannerId = req.query.params.plannerId;
+   const plannerId = req.query.plannerId;
 
   if (!plannerId) {
     return res.status(400).json({ error: "Missing plannerId" });
