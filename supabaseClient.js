864f3caee0ada7ff999997efc85f72c2274788af
AdhamIslam <adhamislam088@gmail.com>
2025-06-20 19:02:51 +0300

    V6.6.2

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.DATABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
